f9207f3d50160502b64e1e28defa15599cab4d7c
Luke <hluke9242@gmail.con>
2023-05-22 14:38:39 -0600

    complete lab phase-1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(p1 = "roller-skate"){
+    return `This Saturday, I want to ${p1}!`
+}
+
+function mondayWork(p2 = "go to the office"){
+    return `This Monday, I will ${p2}.`
+}
+
+function wrapAdjective(string="*"){
+    return function(parameter = "hard worker"){
+        return `You are ${string}${parameter}${string}!`
+    
+    }
+}
